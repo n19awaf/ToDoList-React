1a576cf7c20daf10d33bb02daa909457bf69c38a
n19awaf <nawaf13-@hotmail.com>
2024-02-03 02:41:05 +0800

    last updated

---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,15 @@
+function TodoRowItem(props) {
+
+
+    return (
+        <tr>
+            <th scope='row'>{props.rowNumber}</th>
+            <td>{props.rowDescription}</td>
+            <td>{props.rowAssigned}</td>
+        </tr>
+    )
+
+}
+
+
+export default TodoRowItem;
